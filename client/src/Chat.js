a80d59025a217ebd92f669b92f551eef251a0899
Shelby Rothman <srothman@corelation.local>
2021-09-07 12:48:42 -0700

    succesfully posting and getting messages

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache, useMutation, useSubscription, gql } from '@apollo/client';
 import { WebSocketLink } from "@apollo/client/link/ws";
 import { Container, Chip, Grid, TextField, Button } from '@material-ui/core';
+import React, {useState} from 'react';
 
 //initialize a websocketLink to handle our subscriptions
 const link = new WebSocketLink({
@@ -16,12 +17,45 @@ export const client = new ApolloClient({
     cache: new InMemoryCache(),
 });
 
-export const Chat = () => {
-    return (
-        <div>
-            <h3>Welcome to chatQL!</h3>
-            <Messages />
-        </div>
+export const Chat = () =>{
+    const [user, setUser] = useState("Shelby"); //initialize user
+    const [text, setText] = useState(""); //initialize text
+
+    //Pass our POST_MESSAGE query in the useMutation hook which returns an array, where the first element is our mutate function, postMessage.
+    const [postMessage] = useMutation(POST_MESSAGE)
+    
+    const sendMessage=() => {
+        //sendMessage first checks if both inputs are not empty.
+        if(text.length>0 && user.length >0){
+          // It executes the postMessage mutate function by passing the variables from the input (user and text) and resets the text field.
+          postMessage({
+            variables:{ user: user, text: text }
+          })
+          setText(""); //reset text field
+        }else{
+          // If one of the input fields is blank, an alert window will trigger.
+          alert("Missing fields!")
+        }
+      }
+
+    return(
+        <Container>
+          <h3>Welcome to chatQL!</h3>
+          <Messages/>
+          <Grid container spacing={2}>
+            <Grid item xs={3}>
+              <TextField onChange={ (e) => {
+                setUser(e.target.value)}} value={user} size="small" fullWidth variant="outlined" required label="Required" label="Enter name" />
+            </Grid>
+            <Grid item xs={8}>
+              <TextField onChange={(e)=>{
+                setText(e.target.value)}} value={text} size="small" fullWidth variant="outlined" required label="Required" label="Enter message here" />
+            </Grid>
+            <Grid item xs={1}>
+              <Button onClick={sendMessage} fullWidth  variant="contained" style={{backgroundColor:"#60a820", color:"white"}}>Send</Button>
+            </Grid>
+          </Grid>
+        </Container>
     )
 }
 
@@ -34,6 +68,13 @@ const GET_MESSAGES = gql`
     }
   }
 `;
+
+const POST_MESSAGE = gql`
+  mutation($user:String!, $text:String!){
+    postMessage(user:$user, text:$text)
+  }
+`;
+
 
 const Messages = () => {
     const { data } = useSubscription(GET_MESSAGES);
